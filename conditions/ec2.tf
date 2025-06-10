36f87d5bfa29fecf5e0cfed2fd3765065317e45b
jay-rapolu <jay.rapolu@outlook.com>
2025-06-10 11:22:17 +0530

    added conditions terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,32 @@
+//everything is resource in terraform to create any resource the syntax is:
+//resource "resource_module" "name"{  }
+
+resource "aws_instance" "roboshop" {
+  ami           = var.ami_id
+  instance_type = var.environment == "dev" ? var.instance_type : "t3.small"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+
+  tags = var.instance_tags
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  tags = var.sg_tags
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
---
+++ conditions/variables.tf
@@ -0,0 +1,53 @@
+variable "ami_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type"{
+    type = string
+    default = "t2.micro"
+}
+
+variable "instance_tags"{
+    type = map
+    default = {
+        Name = "roboshop"
+    }
+}
+
+variable "sg_name"{
+    type = string
+    default = "allow_all"
+}
+
+variable "sg_description"{
+    type = string
+    default = "Allow all traffic from internet"
+}
+
+variable "sg_tags"{
+    type = map
+    default = {
+        Name = "allow_all"
+    }
+}
+
+variable "from_port"{
+    type = number
+    default = 0
+}
+
+variable "to_port"{
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks"{
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "environment"{
+    type = string
+    default = "dev"
+}
