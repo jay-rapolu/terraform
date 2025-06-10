e7bf4cf0a1cc614897b5ce2631b90814dabf962e
jay-rapolu <jay.rapolu@outlook.com>
2025-06-10 10:41:55 +0530

    Initiated variables

---
+++ variables/ec2.tf
@@ -0,0 +1,36 @@
+//everything is resource in terraform to create any resource the syntax is:
+//resource "resource_module" "name"{  }
+
+resource "aws_instance" "roboshop" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+
+  tags = {
+    Name = "roboshop"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic from internet"
+
+  tags = {
+    Name = "allow_all"
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
